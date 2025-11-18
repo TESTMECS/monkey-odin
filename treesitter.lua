c8515ec57ae4bc7d1bf2b27d8eb7622ff1d5f51b
TESTMECS <axwar7410@gmail.com>
2025-11-18 13:12:25 -0500

    treesitter.lua

--- treesitter.lua
+++ treesitter.lua
@@ -1,3 +1,4 @@
+-- DEBUG for my tree-sitter-highlights in nvim
 local ok, err = pcall(vim.treesitter.require_language, "monkey")
 print(ok, err)
 -- print(vim.inspect(vim.opt.runtimepath:get()))
@@ -11,3 +12,9 @@ print(vim.inspect(vim.treesitter.language_version))
 print(vim.inspect(vim.api.nvim_get_runtime_file("queries/monkey/*", true)))
 local lang = vim.treesitter.language.get_lang("monkey")
 print(lang)
+-- :lua dofile("treesitter.lua")
+-- vim.filetype.add({
+-- 	extension = {
+-- 		monkey = "monkey",
+-- 	},
+-- })
